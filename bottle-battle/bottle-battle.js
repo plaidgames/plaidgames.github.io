faa1b444824b29082e5c88752e7bf0ad6f92551e
plaidgames <186748263+plaidgames@users.noreply.github.com>
2024-10-29 13:41:19 -0700

    WIP

--- bottle-battle/bottle-battle.js
+++ bottle-battle/bottle-battle.js
@@ -1,12 +1,26 @@
 const bottle = document.getElementById('bottle');
-const spinButton = document.getElementById('spin-button');
-
-const playerAnchors = document.querySelectorAll('.player-name-anchor');
+const playerNameElements = document.querySelectorAll('.player-name');
 
 let angle = 0;
 
-function toggle() {
-	var classList = playerAnchors[0].firstElementChild.classList;
+bottle.addEventListener('transitionend', () => {
+	const winnerIdx = angle % 360 <= 180 ? 0 : 1;
+	const classList = playerNameElements[winnerIdx].classList;
+	classList.add('won');
+	classList.remove('init');
+});
+
+function randomSpin() {
+	playerNameElements[0].classList.remove('won');
+	playerNameElements[1].classList.remove('won');
+
+	const randomDegree = Math.random() * 360 * 2;
+	angle += 4 * 360 + randomDegree;
+	bottle.style.transform = `rotate(${angle}deg)`;
+}
+
+function debugToggleWin() {
+	var classList = playerNameElements[0].classList;
 	if (!classList.contains('won')) {
 		classList.add('won');
 		classList.remove('init');
@@ -15,28 +29,25 @@ function toggle() {
 	}
 }
 
-bottle.addEventListener('click', () => {
-	playerAnchors[0].firstElementChild.classList.remove('won');
-	playerAnchors[1].firstElementChild.classList.remove('won');
+bottle.addEventListener('click', () => randomSpin());
 
-	const randomDegree = Math.random() * 360 * 2;
-	angle += 4 * 360 + randomDegree;
-	bottle.style.transform = `rotate(${angle}deg)`;
-});
-
-bottle.addEventListener('transitionend', () => {
-	const winnerIdx = angle % 360 <= 180 ? 0 : 1;
-	const classList = playerAnchors[winnerIdx].firstElementChild.classList;
-	classList.add('won');
-	classList.remove('init');
-});
-
+let debug = false;
 document.body.addEventListener('keydown', (event) => {
 	if (event.target != document.body) {
 		return;
 	}
 
-	if (event.key == 's') {
-		toggle();
+	if (event.code == 'KeyS') {
+		randomSpin();
+	} else if (event.code == 'KeyW') {
+		debug = true;
+	} else if (event.code == 'Digit1' && debug) {
+		debugToggleWin();
+	} else if (event.code == 'KeyA' && debug) {
+		randomSpin();
+	} else if (event.code == 'KeyD' && debug) {
+		randomSpin();
+	} else if (event.code.startsWith('Key')) {
+		debug = false;
 	}
 });
