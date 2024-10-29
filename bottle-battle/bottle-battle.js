86dba0a0a1fe1249cd3f1bfdaf572c5c1fd69d21
plaidgames <186748263+plaidgames@users.noreply.github.com>
2024-10-29 11:03:04 -0700

    WIP

--- bottle-battle/bottle-battle.js
+++ bottle-battle/bottle-battle.js
@@ -6,24 +6,37 @@ const playerAnchors = document.querySelectorAll('.player-name-anchor');
 let angle = 0;
 
 function toggle() {
-	var classList = greg.firstElementChild.classList;
+	var classList = playerAnchors[0].firstElementChild.classList;
 	if (!classList.contains('won')) {
 		classList.add('won');
+		classList.remove('init');
 	} else {
 		classList.remove('won');
 	}
 }
 
-spinButton.addEventListener('click', () => {
+bottle.addEventListener('click', () => {
 	playerAnchors[0].firstElementChild.classList.remove('won');
 	playerAnchors[1].firstElementChild.classList.remove('won');
 
-	const randomDegree = Math.floor(Math.random() * 360 * 2);
+	const randomDegree = Math.random() * 360 * 2;
 	angle += 4 * 360 + randomDegree;
 	bottle.style.transform = `rotate(${angle}deg)`;
 });
 
 bottle.addEventListener('transitionend', () => {
-	const winnerIdx = (angle + 90) % 360 <= 180 ? 0 : 1;
-	playerAnchors[winnerIdx].firstElementChild.classList.add('won');
+	const winnerIdx = angle % 360 <= 180 ? 0 : 1;
+	const classList = playerAnchors[winnerIdx].firstElementChild.classList;
+	classList.add('won');
+	classList.remove('init');
+});
+
+document.body.addEventListener('keydown', (event) => {
+	if (event.target != document.body) {
+		return;
+	}
+
+	if (event.key == 's') {
+		toggle();
+	}
 });
